34773a4299fec0a761509214597c53b00fe70a8d
John Steward <stewardhere@icloud.com>
2022-07-12 17:09:53 +0200

    Accidentally pressed some keys that made some errors in the previous commit and fixed it

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template,url_for, request
 from flask_sqlalchemy import SQLAlchemy
 
+#instantiating the app and flask knows what to look for
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
 db = SQLAlchemy(app)
-@app.route('/s', methods = ['POST', 'GET'])
+
+
+@app.route('/', methods = ['POST', 'GET']) #root page
+
+@app.route('/home', methods = ['POST', 'GET' ])
 def index():
     if request.method == 'POST':
         return "hello mate"
     else:
-        return render_te
-        mplate('index.html')
+        return render_template('index.html')
 
 
 
