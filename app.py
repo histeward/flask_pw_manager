8942b91863c88b1aeb4ba887ef430899cf06587c
John Steward <stewardhere@icloud.com>
2022-07-13 22:27:17 +0200

    reverting to 34773a4299fec0a761509214597c53b00fe70a8d

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask, render_template,url_for, request
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, url_for, request, redirect
 
 #instantiating the app and flask knows what to look for
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
-db = SQLAlchemy(app)
 
 
 @app.route('/', methods = ['POST', 'GET']) #root page
-
 @app.route('/home', methods = ['POST', 'GET' ])
 def index():
     if request.method == 'POST':
@@ -20,3 +16,4 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+ 
